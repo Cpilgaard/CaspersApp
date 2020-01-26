f65be2f145849b206b7073728562ec7df3fa711e
DanielDKnudsen <daniel.d.knudsen@gmail.com>
2020-01-26 14:45:59 +0100

    added new library for changing IOS-splash screen. Changed background-color of IOS-splashscreen (hopefully works, not testet). Also trimmed appicon

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter_windows_v0.11.3-beta\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Daniel\Documents\Pusterum"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Daniel\Documents\CaspersApp"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
