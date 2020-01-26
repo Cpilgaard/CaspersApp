d0ec8d50fa5d3eba4b6570fd883ec35b5a90c5d9
Daniel <Daniel@DANIEL-PC>
2020-01-26 19:32:19 +0100

    changed appicon

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/casperbakpilgaard/Desktop/pusterum/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/casperbakpilgaard/Desktop/pusterum/CaspersApp"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Daniel\Documents\CaspersApp"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/casperbakpilgaard/Desktop/pusterum/flutter/bin/cache/artifacts/engine/ios-release"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
